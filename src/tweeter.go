e5515d2d4715264719e05179992ec382d83fe0f9
Priscila Monnereau <priscila.monnereau>
2017-11-21 14:12:09 -0300

    agregamos shell interactiva

---
+++ src/tweeter.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/abiosoft/ishell"
+	"github.com/tweeter/src/service"
+)
+
+func main() {
+	shell := ishell.New()
+	shell.SetPrompt("Tweeter >> ")
+	shell.Print("Type help to know commands \n")
+	shell.AddCmd(&ishell.Cmd{
+		Name: "publishTweet",
+		Help: "Publishes a tweet",
+		Func: func(c *ishell.Context) {
+			defer c.ShowPrompt(true)
+			c.Print("Write your tweet")
+			tweet := c.ReadLine()
+			service.PublishTweet(tweet)
+			c.Print("Tweet sent \n")
+			return
+		},
+	})
+
+	shell.AddCmd(&ishell.Cmd{
+		Name: "ShowTweet",
+		Help: "Shows a tweet",
+		Func: func(c *ishell.Context) {
+			defer c.ShowPrompt(true)
+			tweet := service.GetTweet()
+			c.Println(tweet)
+			return
+		},
+	})
+
+	shell.Run()
+}
